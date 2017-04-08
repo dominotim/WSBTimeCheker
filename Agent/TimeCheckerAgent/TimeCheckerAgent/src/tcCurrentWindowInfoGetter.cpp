c8b033770ff01c49de350c411a11724f7302a8af
dominotim <dominotim@mail.ru>
2017-04-08 16:52:52 +0300

    remove tabs

--- Agent/TimeCheckerAgent/TimeCheckerAgent/src/tcCurrentWindowInfoGetter.cpp
+++ Agent/TimeCheckerAgent/TimeCheckerAgent/src/tcCurrentWindowInfoGetter.cpp
@@ -14,46 +14,46 @@ namespace
 {
 std::wstring GetName(IAccessible *pAcc)
 {
-	CComBSTR bstrName;
-	if (!pAcc || FAILED(pAcc->get_accValue(CComVariant((int)CHILDID_SELF), &bstrName)) || !bstrName.m_str)
-		return L"";
-	return bstrName.m_str;
+    CComBSTR bstrName;
+    if (!pAcc || FAILED(pAcc->get_accValue(CComVariant((int)CHILDID_SELF), &bstrName)) || !bstrName.m_str)
+        return L"";
+    return bstrName.m_str;
 }
 
 HRESULT WalkTreeWithAccessibleChildren(CComPtr<IAccessible> pAcc, std::wstring& resUrl)
 {
-	long childCount = 0;
-	long returnCount = 0;
-	HRESULT hr = pAcc->get_accChildCount(&childCount);
-	if (childCount == 0)
-		return S_OK;
+    long childCount = 0;
+    long returnCount = 0;
+    HRESULT hr = pAcc->get_accChildCount(&childCount);
+    if (childCount == 0)
+        return S_OK;
 
-	CComVariant* pArray = new CComVariant[childCount];
-	hr = ::AccessibleChildren(pAcc, 0L, childCount, pArray, &returnCount);
-	if (FAILED(hr))
-		return hr;
-	const std::vector<std::wstring> domens =
-	{ L".ru", L".com", L".org", L".ua" , L".info" };
-	for (int i = 0; i < returnCount; ++i)
-	{
-		CComVariant vtChild = pArray[i];
-		if (vtChild.vt != VT_DISPATCH)
-			continue;
+    CComVariant* pArray = new CComVariant[childCount];
+    hr = ::AccessibleChildren(pAcc, 0L, childCount, pArray, &returnCount);
+    if (FAILED(hr))
+        return hr;
+    const std::vector<std::wstring> domens =
+    { L".ru", L".com", L".org", L".ua" , L".info" };
+    for (int i = 0; i < returnCount; ++i)
+    {
+        CComVariant vtChild = pArray[i];
+        if (vtChild.vt != VT_DISPATCH)
+            continue;
 
-		CComQIPtr<IAccessible> pAccChild = vtChild.pdispVal;
-		if (!pAccChild)
-			continue;
+        CComQIPtr<IAccessible> pAccChild = vtChild.pdispVal;
+        if (!pAccChild)
+            continue;
 
-		const std::wstring name = GetName(pAccChild).data();
-		size_t idx = 0;
-		while (name.find(domens[idx]) == -1 && idx < domens.size()) { ++idx; }
-		if (idx != domens.size())
-			resUrl = name;
-		else if (WalkTreeWithAccessibleChildren(pAccChild, resUrl) == S_FALSE)
-			return S_FALSE;
-	}
-	delete[] pArray;
-	return S_OK;
+        const std::wstring name = GetName(pAccChild).data();
+        size_t idx = 0;
+        while (name.find(domens[idx]) == -1 && idx < domens.size()) { ++idx; }
+        if (idx != domens.size())
+            resUrl = name;
+        else if (WalkTreeWithAccessibleChildren(pAccChild, resUrl) == S_FALSE)
+            return S_FALSE;
+    }
+    delete[] pArray;
+    return S_OK;
 }
 }  // namespace
 
@@ -61,61 +61,61 @@ namespace tcCurrentWindowInfoGetter
 {
 bool IsBrowserWindow(HWND windowHendler)
 {
-	wchar_t className[100];
-	GetClassName(windowHendler, className, 100);
-	return (wcscmp(className, L"Chrome_WidgetWin_1") == 0);
+    wchar_t className[100];
+    GetClassName(windowHendler, className, 100);
+    return (wcscmp(className, L"Chrome_WidgetWin_1") == 0);
 }
 
 std::wstring GetCurrentUrl(HWND windowHendler)
 {
-	IAccessible* ac = NULL;
-	long childCount = 0;
-	long returnCount = 0;
-	HRESULT hr2 = AccessibleObjectFromWindow(windowHendler, OBJID_CLIENT, IID_IAccessible, (void **)&ac);
-	std::wstring res;
-	if (WalkTreeWithAccessibleChildren(ac, res) != S_OK)
-		return L"unrecognized url";
-	return res;
+    IAccessible* ac = NULL;
+    long childCount = 0;
+    long returnCount = 0;
+    HRESULT hr2 = AccessibleObjectFromWindow(windowHendler, OBJID_CLIENT, IID_IAccessible, (void **)&ac);
+    std::wstring res;
+    if (WalkTreeWithAccessibleChildren(ac, res) != S_OK)
+        return L"unrecognized url";
+    return res;
 }
 
 std::wstring GetNormalizedUrl(HWND windowHendler)
 {
-	std::wstring res = GetCurrentUrl(windowHendler);
-	size_t found = res.find(L"://");
-	if (found != std::wstring::npos)
-		res = res.substr(found + 3, res.size());
-	found = res.find(L"/");
-	if (found != std::wstring::npos)
-		return res.substr(0, found);
-	return res;
+    std::wstring res = GetCurrentUrl(windowHendler);
+    size_t found = res.find(L"://");
+    if (found != std::wstring::npos)
+        res = res.substr(found + 3, res.size());
+    found = res.find(L"/");
+    if (found != std::wstring::npos)
+        return res.substr(0, found);
+    return res;
 }
 
 std::wstring GetCurrentApplicationName(HWND windowHendler)
 {
-	DWORD dwPID;
-	GetWindowThreadProcessId(windowHendler, &dwPID);
-	HANDLE handle = OpenProcess(
-		PROCESS_QUERY_INFORMATION | PROCESS_VM_READ,
-		FALSE,
-		dwPID
-	);
-	if(!handle)
-		return L"undefined";
-	wchar_t buffer[MAX_PATH];
-	if (GetModuleFileNameExW(handle, 0, buffer, MAX_PATH))
-	{
-		CloseHandle(handle);
-		return std::wstring(buffer);
-	}
-	return L"undefined";
+    DWORD dwPID;
+    GetWindowThreadProcessId(windowHendler, &dwPID);
+    HANDLE handle = OpenProcess(
+        PROCESS_QUERY_INFORMATION | PROCESS_VM_READ,
+        FALSE,
+        dwPID
+    );
+    if(!handle)
+        return L"undefined";
+    wchar_t buffer[MAX_PATH];
+    if (GetModuleFileNameExW(handle, 0, buffer, MAX_PATH))
+    {
+        CloseHandle(handle);
+        return std::wstring(buffer);
+    }
+    return L"undefined";
 }
 
 std::wstring GetNormalizedCurrentApplicationName(HWND windowHendler)
 {
-	std::wstring res = GetCurrentApplicationName(windowHendler);
-	size_t last = res.find_last_of(L"\\");
-	if(last != std::wstring::npos)
-		return res.substr(last + 1, res.size());
-	return res;
+    std::wstring res = GetCurrentApplicationName(windowHendler);
+    size_t last = res.find_last_of(L"\\");
+    if(last != std::wstring::npos)
+        return res.substr(last + 1, res.size());
+    return res;
 }
 }  // tcCurrentWindowInfoGetter
--- Agent/TimeCheckerAgent/TimeCheckerAgent/src/tcUserInfo.cpp
+++ Agent/TimeCheckerAgent/TimeCheckerAgent/src/tcUserInfo.cpp
@@ -7,20 +7,20 @@ namespace tcUserInfoHelper
 {
 std::wstring GetTime(const std::time_t time)
 {
-	char buffer[80];
-	auto timeinfo = std::localtime(&time);
-	strftime(buffer, sizeof(buffer), "%H:%M:%S", timeinfo);
-	std::string tmp(buffer);
-	return std::wstring(tmp.begin(), tmp.end());
+    char buffer[80];
+    auto timeinfo = std::localtime(&time);
+    strftime(buffer, sizeof(buffer), "%H:%M:%S", timeinfo);
+    std::string tmp(buffer);
+    return std::wstring(tmp.begin(), tmp.end());
 }
 
 std::wstring GetData(const std::time_t time)
 {
-	char buffer[80];
-	auto timeinfo = std::localtime(&time);
-	strftime(buffer, sizeof(buffer), "%d-%m-%Y", timeinfo);
-	std::string tmp(buffer);
-	return std::wstring(tmp.begin(), tmp.end());
+    char buffer[80];
+    auto timeinfo = std::localtime(&time);
+    strftime(buffer, sizeof(buffer), "%d-%m-%Y", timeinfo);
+    std::string tmp(buffer);
+    return std::wstring(tmp.begin(), tmp.end());
 }
 }  // tcUserInfoHelper
 
@@ -28,22 +28,22 @@ namespace tcUserInfo
 {
 
 UserInfo::UserInfo(
-	const std::wstring& name,
-	const std::wstring& applycationName,
-	const std::wstring& url,
-	std::time_t time)
-	: m_name(name), m_applycationName(applycationName), m_url(url), m_time(time)
+    const std::wstring& name,
+    const std::wstring& applycationName,
+    const std::wstring& url,
+    std::time_t time)
+    : m_name(name), m_applycationName(applycationName), m_url(url), m_time(time)
 {
 }
 
 std::wstring UserInfo::ToString() const
 {
-	std::wstring result;
-	result += L"UserName=" + this->m_name + L"&";
-	result += L"AppName=" + this->m_applycationName + L"&";
-	result += L"URL=" + this->m_url + L"&";
-	result += std::wstring(L"Date=") + tcUserInfoHelper::GetData(this->m_time) + L"&";
-	result += std::wstring(L"Time=") + tcUserInfoHelper::GetTime(this->m_time);
-	return result;
+    std::wstring result;
+    result += L"UserName=" + this->m_name + L"&";
+    result += L"AppName=" + this->m_applycationName + L"&";
+    result += L"URL=" + this->m_url + L"&";
+    result += std::wstring(L"Date=") + tcUserInfoHelper::GetData(this->m_time) + L"&";
+    result += std::wstring(L"Time=") + tcUserInfoHelper::GetTime(this->m_time);
+    return result;
 }
 }  // tcUserInfo
